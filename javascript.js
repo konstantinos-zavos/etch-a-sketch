4b7e8ec2d7f05e8a7f47fa14a242677f345f1757
koszav <zavos@tuta.io>
2022-11-24 16:00:31 +0200

    Make the hover class stay after clicking

--- javascript.js
+++ javascript.js
@@ -1,18 +1,17 @@
 const container = document.querySelector('#container');
 
-for ( i = 0; i < 255; i++) {
+for ( i = 0; i < 256; i++) {
     const singleSquare = document.createElement('div');
     singleSquare.classList.add('squares');
     container.appendChild(singleSquare);
 }
 
-const toggleColor = container.querySelectorAll('squares');
-
-// we use the .forEach method to iterate through each button
-toggleColor.forEach((squares) => {
+const squares = document.querySelectorAll(".squares");
+squares.forEach((squares) => {
 
   // and for each one we add a 'click' listener
   squares.addEventListener('click', () => {
-    alert("works")
+    squares.classList.remove('squares');
+    squares.classList.add('squares-black');
   });
 });
