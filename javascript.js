8202e09cfa95559b93ec0afd99a687a64ecf3485
koszav <zavos@tuta.io>
2022-11-24 16:25:23 +0200

    Add mouseover support and add a slight transition to leave a trail behind

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ const squares = document.querySelectorAll(".squares");
 squares.forEach((squares) => {
 
   // and for each one we add a 'click' listener
-  squares.addEventListener('click', () => {
+  squares.addEventListener('mouseover', () => {
     squares.classList.remove('squares');
     squares.classList.add('squares-black');
   });
