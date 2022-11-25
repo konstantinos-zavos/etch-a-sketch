4bd356c3c52bf8ad3fdfbb7104088e47980fb25c
koszav <zavos@tuta.io>
2022-11-25 12:57:33 +0200

    Add code for rgb on javascript for testing porpuses

--- javascript.js
+++ javascript.js
@@ -26,3 +26,10 @@ squares.forEach((squares) => {
 function refreshPage() {
   window.location.reload();
 }
+
+
+const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+const r = randomBetween(0, 255);
+const g = randomBetween(0, 255);
+const b = randomBetween(0, 255);
+const rgb = `rgb(${r},${g},${b})`;
