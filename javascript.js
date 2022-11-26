af0e492162272481731a99bfc57e41c20dfa54bb
koszav <zavos@tuta.io>
2022-11-26 19:22:49 +0200

    Fix CSS attributes of squares by removing border and increasing height and width

--- javascript.js
+++ javascript.js
@@ -28,8 +28,32 @@ function refreshPage() {
 }
 
 
-const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-const r = randomBetween(0, 255);
-const g = randomBetween(0, 255);
-const b = randomBetween(0, 255);
-const rgb = `rgb(${r},${g},${b})`;
+
+
+// RGB code - Might use it later
+
+// const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+// const r = randomBetween(0, 255);
+// const g = randomBetween(0, 255);
+// const b = randomBetween(0, 255);
+// const rgb = `rgb(${r},${g},${b})`;
+
+// function rainbow() {
+// const rainbow = document.getElementById('rainbow');
+// rainbow.addEventListener('click', () => {
+//     const element = document.querySelector('.squares-black');
+//     element.style.backgroundColor = rgb;
+// })
+// }
+
+// Another RBG code snippet
+
+
+// let colors = ['#ff0000', '#00ff00', '#0000ff'];
+// let random_color = colors[Math.floor(Math.random() * colors.length)];
+// document.getElementById('squares-black').style.color = random_color;
+
+// Skeftome na allaksw ton tropo pou allazo to xroma - px na min eine me div change, alla apla me 
+// me javascirpt na allazei to xroma apo aspro se mavro
+// meta na kanw to grayscale me kapoio function pou tha ginete activate me to button
+// kai meta to rgb pou exw 2 viable codes
